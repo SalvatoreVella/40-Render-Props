94e4db1dd98cd9047f021c5683506f98a24e237c
Salvatore Vella <salvatorevella1234@gmail.com>
2023-01-06 19:15:13 +0100

    Exercise complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import TodoList from "./TodoList"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <TodoList render={(items, handleDeleteTodo) => {
+      console.log(items);
+      return (
+      <ul>
+        {items.map((item) => {
+            return (
+                    <li key={item.key}>{item.description} <span>{item.done ? "Completato" : ""}</span> <button id={item.key} onClick={handleDeleteTodo}>Delete Todo</button></li>
+            )
+        })}
+    </ul>)
+    }}></TodoList>
   );
 }
 
